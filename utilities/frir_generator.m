c13441a07144dc5d1f1a1f66a9582370ea13cddb
Edward Ly <edward.ly@pm.me>
2019-07-04 14:34:08 +0900

    Updated comments and reference, enforce LF line endings

--- utilities/frir_generator.m
+++ utilities/frir_generator.m
@@ -1,5 +1,5 @@
 function [h, beta_hat] = frir_generator(c, fs, r, s, L, beta, nSamples)
-% FRIR_GENERATOR Generates an impulse response from the given parameters.
+% FRIR_GENERATOR Generates a room impulse response from the given parameters.
 % c = speed of sound (m/s)
 % fs = sampling frequency of impulse response
 % r = 1 x 3 array specifying the (x,y,z) coordinates of receiver (m)
@@ -9,12 +9,16 @@ function [h, beta_hat] = frir_generator(c, fs, r, s, L, beta, nSamples)
 % nSamples = total number of samples in impulse response
 % h = nsample x 1 column vector containing the calculated room impulse response
 % beta_hat = reflection coefficient of the walls based on reverberation time
-% Current algorithm: based on the fast image method by McGovern.
-    % Calculate reflection coefficient
-	if beta > 0
+% Current algorithm: based on the fast image method by McGovern [1].
+%
+% [1] Stephen G. McGovern, Fast image method for impulse response calculations
+% of box-shaped rooms, Applied Acoustics 70 (2009), 182-189.
+%
+  % Calculate reflection coefficient via Sabin-Franklin's formula
+  if beta > 0
         V = L(1) * L(2) * L(3); % room volume
         S = 2 * (L(1)*L(3) + L(2)*L(3) + L(1)*L(2)); % room surface area
-        alfa = 24 * V * log(10.0) / (c * S * beta); % Sabin-Franklin's formula
+        alfa = 24 * V * log(10.0) / (c * S * beta);
         if alfa > 1
             error(['Error: Reflection coefficient cannot be calculated ' ...
                 'using the current room parameters, i.e. room size and ' ...
@@ -28,12 +32,12 @@ function [h, beta_hat] = frir_generator(c, fs, r, s, L, beta, nSamples)
 
     % Calculate total time of impulse response in seconds
     time = nSamples / fs;
-    
+
     % Find highest-order reflection to be used in lookup tables
     xMaxOrder = ceil(c * time / (2 * L(1)));
     yMaxOrder = ceil(c * time / (2 * L(2)));
     zMaxOrder = ceil(c * time / (2 * L(3)));
-    
+
     % Create lookup tables for each dimension
     [xDistSq, xRefCoef] = make_tables( ...
         L(1), r(1), s(1), beta_hat, beta_hat, xMaxOrder);
@@ -41,10 +45,10 @@ function [h, beta_hat] = frir_generator(c, fs, r, s, L, beta, nSamples)
         L(2), r(2), s(2), beta_hat, beta_hat, yMaxOrder);
     [zDistSq, zRefCoef] = make_tables( ...
         L(3), r(3), s(3), beta_hat, beta_hat, zMaxOrder);
-    
+
     % Initialize impulse response array
     h = zeros(nSamples, 1);
-    
+
     i = 1;
     while i <= 2*xMaxOrder + 1
         j = 1;
@@ -53,29 +57,29 @@ function [h, beta_hat] = frir_generator(c, fs, r, s, L, beta, nSamples)
             while k <= 2*zMaxOrder + 1
                 % Find distance from ijk^th image to receiver
                 dist = sqrt(xDistSq(i) + yDistSq(j) + zDistSq(k));
-                
+
                 % Find sample in impulse response corresponding to distance
                 samp = round(dist * fs / c) + 1;
-                
+
                 % If echo from ijk^th image occurs at a time beyond range of
                 % impulse response, then all images in one strip of circle have
                 % been tested.
                 if samp > nSamples, break; end
-                
+
                 % Add echo to impulse response array
                 h(samp) = h(samp) + ...
                     (xRefCoef(i) * yRefCoef(j) * zRefCoef(k) / dist);
-                
+
                 k = k + 1;
             end
-            
+
             % If k-loop broke with k == 1, then all images in "circle of
             % interest" in i-plane have been tested, so no need to find
             % distances for higher values of j.
             if k == 1, break; end
             j = j + 1;
         end
-        
+
         % If j-loop broke with j == 1, then all images in "sphere of interest"
         % have been tested, so no need to find distances for higher values of i.
         if j == 1, break; end
@@ -100,7 +104,7 @@ function [distSq, refCoef] = make_tables(w, r, s, Bmw, Bw, n)
     % Set initial values of left/right image distance sequences
     LimgD = DirSnd;
     RimgD = DirSnd;
-    
+
     % Create tables
     for i = 2:(n + 1)
         % Set image distances and reflection coefficients for i^th order images
@@ -116,7 +120,7 @@ function [distSq, refCoef] = make_tables(w, r, s, Bmw, Bw, n)
             Limg = Limg * Bw;
             Rimg = Rimg * Bmw;
         end
-        
+
         % Determine pattern for i^th order echo pair and set values for both
         % tables
         if (mod(i, 2) ~= 0 && r + s < 0) || (mod(i, 2) == 0 && r - s < 0)
