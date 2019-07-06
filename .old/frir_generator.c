dbdf1e8e169c788ae4c284997db918180ca5e7dd
Edward Ly <edward.ly@pm.me>
2019-07-06 13:31:05 +0900

    Updated license

--- .old/frir_generator.c
+++ .old/frir_generator.c
@@ -10,22 +10,7 @@ Description : Calculates the impulse response of a box-shaped room using the
               rooms, Applied Acoustics 70 (2009), 182-189.
 
 Author      : Edward Ly (m5222120@u-aizu.ac.jp)
-
 Version     : 0.1.0
-
-This program is free software; you can redistribute it and/or modify
-it under the terms of the GNU General Public License as published by
-the Free Software Foundation; either version 2 of the License, or
-(at your option) any later version.
-
-This program is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU General Public License for more details.
-
-You should have received a copy of the GNU General Public License
-along with this program; if not, write to the Free Software
-Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 */
 
 #include <stdio.h>
