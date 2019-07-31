5f7a57635f2fe7a7d8d5935025274940bc5ee9b2
Edward Ly <edward.ly@pm.me>
2019-07-31 11:43:47 +0900

    Removed restriction on crossover point

--- components/crossover.m
+++ components/crossover.m
@@ -2,8 +2,8 @@ function out = crossover(in, SELECTION_SIZE, POPULATION_SIZE, NUM_SAMPLES)
 % CROSSOVER Generate children and replace least fit individuals.
 % out = output population
 % in = input population
-% Current algorithm: one-point crossover where the point is within the noise
-% threshold for both impulse responses.
+% Current algorithm: one-point crossover where the point is random along the
+% length of the impulse response.
     % Require all arguments
     if nargin < 4, error('Not enough input arguments.'); end
 
@@ -12,10 +12,7 @@ function out = crossover(in, SELECTION_SIZE, POPULATION_SIZE, NUM_SAMPLES)
     for i = (SELECTION_SIZE + 1):POPULATION_SIZE
         parents = randperm(SELECTION_SIZE, 2);
         point = ceil(rand * (NUM_SAMPLES - 1));
-        while in(point, parents(1)) ~= 0 || in(point, parents(2)) ~= 0
-            point = ceil(rand * (NUM_SAMPLES - 1));
-        end
-        
+
         out(1:point, i) = in(1:point, parents(1));
         out((point + 1):end, i) = in((point + 1):end, parents(2));
     end
