e10acf361fb8996523570812c320b5e82bfa1430
Edward Ly <edward.ly@pm.me>
2019-08-05 20:59:27 +0900

    Updated initialization algorithm

--- functions/init_pop.m
+++ functions/init_pop.m
@@ -1,33 +1,33 @@
 function pop = init_pop(NUM_SAMPLES, POPULATION_SIZE, SAMPLE_RATE, T60)
 % INIT_POP Generate an initial population.
 % pop = output population
-% Current algorithm: use rir_generator by Habets to generate simulated rooms.
-% The receiver, source, and room parameters are also randomly generated for each
+% Current algorithm: use the fast RIR generator by McGovern to generate rooms.
+% The receiver, source, and room parameters are randomly determined for each
 % individual.
+% The room dimensions are biased towards higher values, with a maximum limit
+% automatically determined from T60.
     % Require all arguments
     if nargin < 4, error('Not enough input arguments.'); end
 
     pop = zeros(NUM_SAMPLES, POPULATION_SIZE);
 
+    c = 340;                    % Sound velocity (m/s)
+    fs = SAMPLE_RATE;           % Sample frequency (samples/s)
+    beta = T60;                 % T60 Reverberation time (s)
+    n = NUM_SAMPLES;            % Total reverberation time (samples)
+    
+    % Determine upper limit of room dimensions
+    maxL = c * beta / (4 * log(10.0));
+    
     for i = 1:POPULATION_SIZE
-        % Randomly set room dimensions and receiver/source positions.
-        roomDims = abs(randn(1, 3)) .* 10 + 5;
-        rPos = [ ...
-            rand * roomDims(1, 1), ...
-            rand * roomDims(1, 2), ...
-            rand * roomDims(1, 3)];
-        sPos = [ ...
-            rand * roomDims(1, 1), ...
-            rand * roomDims(1, 2), ...
-            rand * roomDims(1, 3)];
-
-        c = 340;                    % Sound velocity (m/s)
-        fs = SAMPLE_RATE;           % Sample frequency (samples/s)
-        r = rPos;                   % Receiver position [x y z] (m)
-        s = sPos;                   % Source position [x y z] (m)
-        L = roomDims;               % Room dimensions [x y z] (m)
-        beta = T60;                 % T60 Reverberation time (s)
-        n = NUM_SAMPLES;            % Total reverberation time (samples)
+        % Room dimensions [x y z] (m)
+        L = (1 - rand(1, 3) .^ 2) .* maxL;
+        
+        % Receiver position [x y z] (m)
+        r = [rand * L(1, 1), rand * L(1, 2), rand * L(1, 3)];
+        
+        % Source position [x y z] (m)
+        s = [rand * L(1, 1), rand * L(1, 2), rand * L(1, 3)];
 
         pop(:, i) = frir_generator(c, fs, r, s, L, beta, n);
     end
