f9e5b74bbc1ea19b32a6b9a687dd72f71c385051
Edward Ly <edward.ly@pm.me>
2019-11-11 01:25:20 +0900

    Updated comments, simplified clarity calculation

--- components/fitness.m
+++ components/fitness.m
@@ -19,45 +19,48 @@ function f = fitness(ir, params)
     if nargin < 2, error('Not enough input arguments.'); end
     if nargout < 1, error('Not enough output arguments.'); end
 
-    % Copy sample rate to new variable (to shorten name).
+    %% Useful information for parameter calculations
+    % Copy sample rate to new variable (to shorten name)
     sampleRate = params.SAMPLE_RATE;
 
-    % Get start time of IR (arrival of peak reflection).
+    % Get start time of IR (arrival of peak reflection)
     [~, irMaxIndex] = max(abs(ir));
 
+    % Calculate Schroeder curve of impulse response
+    [irEDC, irEDCdB] = schroeder(ir);
+
+    %% Parameter calculations
     % ITDG (initial time delay gap)
-    % Calculate time difference between first two arrivals.
+    % Calculate time difference between first two arrivals
     irIndices = find(ir, 2);
     if numel(irIndices) < 2, f = Inf; return; end
     irITDG = (irIndices(2) - irIndices(1)) / sampleRate;
 
     % T60
-    % Calculate T30 (time from -5 to -35 dB) and multiply by 2.
-    irEDC = schroeder(ir, 'dB');
-    irEDCMaxLevel = irEDC(irMaxIndex);
-    ir5dBSample = find(irEDC - irEDCMaxLevel < -5, 1);
+    % Calculate T30 (time from -5 to -35 dB) and multiply by 2
+    irEDCMaxLevel = irEDCdB(irMaxIndex);
+    ir5dBSample = find(irEDCdB - irEDCMaxLevel < -5, 1);
     if isempty(ir5dBSample), f = Inf; return; end
-    ir35dBSample = find(irEDC - irEDCMaxLevel < -35, 1);
+    ir35dBSample = find(irEDCdB - irEDCMaxLevel < -35, 1);
     if isempty(ir35dBSample), f = Inf; return; end
     irT60 = (ir35dBSample - ir5dBSample) * 2 / sampleRate;
 
     % EDT (early decay time, i.e. time from 0 to -10 dB)
-    ir10dBSample = find(irEDC - irEDCMaxLevel < -10, 1);
+    ir10dBSample = find(irEDCdB - irEDCMaxLevel < -10, 1);
     if isempty(ir10dBSample), f = Inf; return; end
     irEDT = (ir10dBSample - irMaxIndex) / sampleRate;
 
     % C80 (clarity)
     sample_80ms = floor(0.08 * sampleRate) + irMaxIndex;
     if sample_80ms >= numel(ir), f = Inf; return; end
-    earlyReflections = ir(1:sample_80ms);
-    lateReflections  = ir((sample_80ms + 1):end);
-    earlyEnergy = sum(earlyReflections .* earlyReflections);
-    lateEnergy  = sum(lateReflections .* lateReflections);
+    earlyEnergy = irEDC(irMaxIndex) - irEDC(sample_80ms);
+    lateEnergy  = irEDC(sample_80ms);
+    if lateEnergy <= 0, f = Inf; return; end
     irC80 = 10 * log10(earlyEnergy / lateEnergy);
 
     % BR (bass ratio)
     % Find amount of energy in 125 - 500Hz and 500Hz - 2000Hz bands and
-    % calculate the ratio of the two.
+    % calculate the ratio of the two
     irfft = abs(fft(ir));
     freq = (0:length(irfft)-1) * sampleRate / length(irfft);
     f125 = ceil(125 * length(irfft) / sampleRate) + 1;
@@ -69,7 +72,8 @@ function f = fitness(ir, params)
     highEnergy = sum(highContent .* highContent);
     irBR = lowEnergy / highEnergy;
     
-    % Calculate the mean squared error.
+    %% Calculate fitness value
+    % Calculate mean squared error
     T60diff  = irT60  - params.T60;
     ITDGdiff = irITDG - params.ITDG;
     EDTdiff  = irEDT  - params.EDT;
