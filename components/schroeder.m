173fcf954c6373a0fb9332cc428c9fb3968eae94
Edward Ly <edward.ly@pm.me>
2019-11-11 00:57:53 +0900

    Added Schroeder curve in decibels as 2nd output argument

--- components/schroeder.m
+++ components/schroeder.m
@@ -1,14 +1,14 @@
-function out = schroeder(in, mode)
+function [out, outdB] = schroeder(in)
 % SCHROEDER Calculates the energy decay (Schroeder) curve of impulse response.
 %
 % Input arguments:
 % in = vector containing impulse response energy
-% mode = if set to 'dB', convert values to decibel units (optional)
 %
 % Output arguments:
 % out = vector containing energy decay curve
+% outdB = vector containing energy decay curve in decibels
 %
-    % Require all arguments
+    % Require input argument and one output argument
     if nargin < 1, error('Not enough input arguments.'); end
     if nargout < 1, error('Not enough output arguments.'); end
 
@@ -17,5 +17,5 @@ function out = schroeder(in, mode)
         out(i) = out(i) + out(i + 1);
     end
 
-    if nargin > 1 && strcmp(mode, 'dB'), out = 10 .* log10(out); end
+    if nargout > 1, outdB = 10 .* log10(out); end
 end
