51312631b992b86cb741a349e862bbbbaa4b1ac0
Edward Ly <edward.ly@pm.me>
2019-11-21 01:40:48 +0900

    Tag plugin version 2.1.1

--- components/GeneticReverb.m
+++ components/GeneticReverb.m
@@ -5,8 +5,8 @@
 %
 % File: GeneticReverb.m
 % Author: Edward Ly (m5222120@u-aizu.ac.jp)
-% Version: 2.1.0
-% Last Updated: 12 November 2019
+% Version: 2.1.1
+% Last Updated: 21 November 2019
 %
 % Usage: Validate and generate the VST plugin, respectively, with:
 %     validateAudioPlugin GeneticReverb
