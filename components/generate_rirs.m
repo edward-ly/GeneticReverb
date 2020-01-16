d949336efda5575e86dbdb1861b541b7a8e61a7e
Edward Ly <edward.ly@pm.me>
2020-01-16 23:25:18 +0900

    Removed normalize plugin parameter, updated UI layout

--- components/generate_rirs.m
+++ components/generate_rirs.m
@@ -66,12 +66,10 @@ function [irLeft, irRight] = generate_rirs(plugin, sampleRate)
         % Apply low-shelf EQ
         newIRs = shelf_filt(newIRs, plugin.WARMTH, plugin.IR_SAMPLE_RATE);
 
-        if plugin.NORMALIZE
-            % Modify gains of IRs so that RMS levels are equal
-            newIRsRMS = rms(newIRs);
-            newIRs(:, 1) = newIRs(:, 1) .* (1 + (newIRsRMS(2) / newIRsRMS(1)));
-            newIRs(:, 2) = newIRs(:, 2) .* (1 + (newIRsRMS(1) / newIRsRMS(2)));
-        end
+        % Modify gains of IRs so that RMS levels are equal
+        newIRsRMS = rms(newIRs);
+        newIRs(:, 1) = newIRs(:, 1) .* (1 + (newIRsRMS(2) / newIRsRMS(1)));
+        newIRs(:, 2) = newIRs(:, 2) .* (1 + (newIRsRMS(1) / newIRsRMS(2)));
 
         % Normalize to prevent clipping
         newIRs = normalize_signal(newIRs, 0.99);
