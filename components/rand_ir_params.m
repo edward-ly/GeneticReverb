a2e89876aa031fdd079950e6d8b081972cb026fb
Edward Ly <edward.ly@pm.me>
2020-01-22 18:27:39 +0900

    Fixed warmth parameter range for evaluation

--- components/rand_ir_params.m
+++ components/rand_ir_params.m
@@ -20,7 +20,7 @@ function params = rand_ir_params(T60)
         'EDT', 0, ...
         'ITDG', 0.001 * (100 ^ rand), ... % 0.001 to 0.1 s (log)
         'C80', rand * 10 - 5, ...         % -5 to 5 dB (lin)
-        'BR', 0.5 * 4 ^ rand);            % 0.5 to 2 (log)
+        'BR', rand * 10 - 5);             % -5 to 5 dB (lin)
 
     params.NUM_SAMPLES = ceil(1.5 * params.T60 * params.SAMPLE_RATE);
     params.EDT = params.T60 / 6.0;
