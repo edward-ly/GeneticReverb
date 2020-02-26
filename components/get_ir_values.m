56f550cc27718560fcd939442a98b42d160b9823
Edward Ly <edward.ly@pm.me>
2020-02-26 15:14:45 +0900

    Added script for calculating acoustics values from IR file

---
+++ components/get_ir_values.m
@@ -0,0 +1,85 @@
+function irValues = get_ir_values(ir, numSamples, sampleRate)
+% GET_IR_VALUES Returns the acoustics parameter values of an impulse response.
+%
+% Input arguments:
+% ir = column vector containing impulse response
+% numSamples = length of impulse response
+% sampleRate = sample rate of impulse response
+%
+% Output arguments:
+% irValues = struct containing IR parameter values
+%     T60 = T60 decay time (s)
+%     EDT = early decay time (s)
+%     ITDG = initial time delay gap (s)
+%     PREDELAY = predelay time (s)
+%     C80 = clarity (dB)
+%     BR = bass ratio (dB)
+%
+    % Require all input arguments
+    if nargin < 3, error('Not enough input arguments.'); end
+
+    % =========================================================================
+
+    % Filter for peaks and valleys
+    irdB = 20 .* log10(abs(ir));
+    [~, peaks] = findpeaks(irdB);
+    filteredIR = zeros(numSamples, 1);
+    filteredIR(peaks) = ir(peaks);
+
+    % Find onset of impulse response and zero out previous samples
+    [~, irInitSample] = max(abs(filteredIR));
+    filteredIR(1:irInitSample) = 0;
+
+    % Calculate Schroeder curve of impulse response, set initial sample to 0 dB
+    [irEDC, irEDCdB] = schroeder(filteredIR);
+    irEDCdB = irEDCdB - max(irEDCdB);
+
+    % =========================================================================
+
+    % ITDG (initial time delay gap) & predelay
+    irITDGSample = find(irEDCdB < 0 & irEDCdB > -45, 1);
+    irITDG = (irITDGSample - irInitSample) / sampleRate;
+    irDelay = irInitSample / sampleRate;
+
+    % T60 & EDT
+    % T60 = Calculate T30 (time from -5 to -35 dB) and multiply by 2
+    % EDT = Calculate time from 0 to -10 dB
+    ir5dBSample = find(irEDCdB < -5, 1);
+    if isempty(ir5dBSample), ir5dBSample = -Inf; end
+
+    ir10dBSample = find(irEDCdB < -10, 1);
+    if isempty(ir10dBSample), ir10dBSample = Inf; end
+
+    ir35dBSample = find(irEDCdB < -35, 1);
+    if isempty(ir35dBSample), ir35dBSample = Inf; end
+
+    irT60 = (ir35dBSample - ir5dBSample) * 2 / sampleRate;
+    irEDT = (ir10dBSample - irInitSample) / sampleRate;
+
+    % C80 (clarity)
+    sample_80ms = round(0.08 * sampleRate) + irInitSample;
+    % if sample_80ms >= irParams.NUM_SAMPLES, f = Inf; return; end
+    earlyEnergy = irEDC(irInitSample) - irEDC(sample_80ms);
+    lateEnergy  = irEDC(sample_80ms);
+    % lateEnergy(lateEnergy <= 0) = Inf;
+    irC80 = 10 .* log10(earlyEnergy ./ lateEnergy);
+
+    % BR (bass ratio)
+    % Find amount of energy in 125 - 500Hz and 500Hz - 2000Hz bands and
+    % calculate the ratio between the two
+    irfft = 20 .* log10(abs(fft(filteredIR)));
+    f125 = ceil(125 * numSamples / sampleRate) + 1;
+    f500 = ceil(500 * numSamples / sampleRate);
+    f2000 = floor(2000 * numSamples / sampleRate) + 1;
+    lowContent = mean(irfft(f125:f500));
+    highContent = mean(irfft((f500 + 1):f2000));
+    irBR = lowContent - highContent;
+
+    irValues = struct( ...
+        'T60', irT60, ...
+        'EDT', irEDT, ...
+        'ITDG', irITDG, ...
+        'PREDELAY', irDelay, ...
+        'C80', irC80, ...
+        'BR', irBR);
+end
---
+++ scripts/evaluate_ir.m
@@ -0,0 +1,58 @@
+% Script to calculate the acoustics parameter values of an impulse response.
+%
+% File: evaluate_ir.m
+% Author: Edward Ly (m5222120@u-aizu.ac.jp)
+% Version: 0.1.0
+% Last Updated: 18 February 2020
+%
+% BSD 3-Clause License
+%
+% Copyright (c) 2019, Edward Ly
+% All rights reserved.
+%
+% Redistribution and use in source and binary forms, with or without
+% modification, are permitted provided that the following conditions are met:
+%
+% 1. Redistributions of source code must retain the above copyright notice, this
+%    list of conditions and the following disclaimer.
+%
+% 2. Redistributions in binary form must reproduce the above copyright notice,
+%    this list of conditions and the following disclaimer in the documentation
+%    and/or other materials provided with the distribution.
+%
+% 3. Neither the name of the copyright holder nor the names of its
+%    contributors may be used to endorse or promote products derived from
+%    this software without specific prior written permission.
+%
+% THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+% AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+% IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+% DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+% FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+% DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+% SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+% CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+% OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+% OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+%% Preamble
+% Clear workspace and figures
+clear; close all;
+
+% Add paths to any external functions used
+addpath ../components
+
+%% Choose an audio file for input
+[fileName, filePath] = uigetfile( ...
+    {'*.wav', 'WAV Files (*.wav)'}, 'Open WAV File...');
+if ~fileName, fprintf('No file selected, exiting...\n'); return; end
+
+[drySignal, audioSampleRate] = audioread([filePath fileName]);
+[numAudioSamples, numAudioChannels] = size(drySignal);
+
+%% Calculate and display parameter values
+fprintf('Analyzing impulse response in file "%s%s"...\n', filePath, fileName);
+for i = 1:numAudioChannels
+    fprintf('Channel %i:\n', i);
+    get_ir_values(drySignal(:, i), numAudioSamples, audioSampleRate)
+end
